2aa6d76015ab92b4b08ee2448ac2ab6ceb21da53
Oscar Sultanov <oscars@live.ru>
2021-03-24 12:33:50 +0500

    Misc types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,8 @@
+export declare type LanguageCode = string;
+export declare type TemplateData = Record<string, unknown>;
+export declare type Template = (data: Readonly<TemplateData>) => string;
+export declare type RepositoryEntry = Record<string, Template>;
+export declare type Repository = Record<LanguageCode, Readonly<RepositoryEntry>>;
 interface Config {
   directory?: string;
   useSession?: boolean;
@@ -9,27 +14,41 @@ interface Config {
 
 type ContextUpdate = (ctx: any, next?: (() => any) | undefined) => any;
 
-export class I18n {
-  constructor(input: Config);
-  loadLocales(directory: string): void;
-  loadLocale(languageCode: string, i18Data: object): void;
-  resetLocale(languageCode: string): void;
-  availableLocales(): string[];
-  resourceKeys(languageCode: string): string[];
-  missingKeys(languageOfInterest: string, referenceLanguage?: string): string[];
-  overspecifiedKeys(languageOfInterest: string, referenceLanguage?: string): string[];
-  translationProgress(languageOfInterest: string, referenceLanguage?: string): number;
-  middleware(): ContextUpdate;
-  createContext(languageCode: string, templateData: object): void;
-  t(languageCode?: string, resourceKey?: string, templateData?: object): string;
-  t(resourceKey?: string, templateData?: object): string;
+export declare class I18nContext {
+  readonly config: Config;
+  readonly repository: Repository;
+  readonly templateData: Readonly<TemplateData>;
+  languageCode: string;
+  shortLanguageCode: string;
+  constructor(repository: Readonly<Repository>, config: Config, languageCode: string, templateData: Readonly<TemplateData>);
   locale(): string;
-  locale(languageCode?: string): void;
+  locale(languageCode: string): void;
+  getTemplate(languageCode: string, resourceKey: string): Template | undefined;
+  t(resourceKey: string, templateData?: Readonly<TemplateData>): string;
 }
+
+export declare class I18n {
+  repository: Repository;
+  readonly config: Config;
+  constructor(config?: Partial<Config>);
+  loadLocales(directory: string): void;
+  loadLocale(languageCode: LanguageCode, i18nData: Readonly<Record<string, unknown>>): void;
+  resetLocale(languageCode?: LanguageCode): void;
+  availableLocales(): LanguageCode[];
+  resourceKeys(languageCode: LanguageCode): string[];
+  missingKeys(languageOfInterest: LanguageCode, referenceLanguage?: string): string[];
+  overspecifiedKeys(languageOfInterest: LanguageCode, referenceLanguage?: string): string[];
+  translationProgress(languageOfInterest: LanguageCode, referenceLanguage?: string): number;
+  createContext(languageCode: LanguageCode, templateData: Readonly<TemplateData>): I18nContext;
+  middleware(): ContextUpdate;
+  t(languageCode: LanguageCode, resourceKey: string, templateData?: Readonly<TemplateData>): string;
+}
+
+export declare function match(resourceKey: string, templateData?: Readonly<TemplateData>): (text: string, ctx: any) => RegExpExecArray | null;
 
 export default I18n;
 declare module 'telegraf' {
   export interface Context {
-    i18n: I18n
+    i18n: I18nContext
   }
 }
